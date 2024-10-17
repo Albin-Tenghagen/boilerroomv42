2f705db808fc9face211505f4ba65375c744b467
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2024-10-17 12:56:40 +0200

    kalkylator tillagd

---
+++ js/index.js
@@ -0,0 +1,53 @@
+function kalkylator() {
+    let firstInput = parseFloat(prompt("första siffran"));
+    let secondInput = parseFloat(prompt("andra siffran"));
+    
+    
+    if (isNaN(firstInput) || isNaN(secondInput)) {
+       alert("Felaktig inmatning. Var vänlig mata in giltiga nummer.");
+           return;
+    }
+    
+    
+    
+    
+     let firstValue = Number(firstInput)
+    let secondValue = Number(secondInput)
+    
+    
+    let operation = prompt("välj en operation (+, -, /, *)")
+    
+    
+    let result;
+    
+    
+    switch (operation) {
+       case '+':
+           result = firstInput + secondInput;
+       break;
+       case '-':
+           result = firstInput - secondInput;
+       break;
+       case '/':
+           if (secondInput === 0) {
+               alert('Kan inde dividera med noll!')
+               return;
+           }
+           result = firstInput / secondInput;
+       break;
+       case '*':
+           result = firstInput * secondInput;
+       break;
+       default:
+       alert('ogiltigt räknesätt, försök igen')
+       return;
+    }
+    
+    
+    alert(`Resultatet av ${firstInput} ${operation} ${secondInput} är: ${result}`);
+    
+    
+    }
+    
+    
+    kalkylator()
