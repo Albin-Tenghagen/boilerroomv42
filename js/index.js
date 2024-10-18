b942d50affd511519020ef5b60fd0d3965de2f60
ZanaPK0 <timezana10@gmail.com>
2024-10-18 11:09:02 +0200

    its working

--- js/index.js
+++ js/index.js
@@ -1,122 +1,97 @@
-// function sparadSiffraEtt() {
-//     let siffraEtt = document.getElementById("siffra1").value;
-//     // alert("The user typed '" + siffraEtt + "'");
-//     console.log(siffraEtt);
-// }
+// const operator = document.getElementById("operator");
+
+function sparaSiffra1(){
+let firstInput = document.getElementById('siffra1').value;
+console.log(firstInput);
+return firstInput;
+}
+
+function operator1(){
+    let operator = document.getElementById('operator').value;
+    console.log(operator);
+    return operator;
+}
+
+ function sparaSiffra2(){
+   let secondInput = document.getElementById('siffra2').value;
+   console.log(secondInput);
+   return secondInput; 
+}
+
+
+
+
+
+// const firstInput = document.getElementById("siffra1");
+// const inputValue = firstInput.value;
+
+
+
+
+// const secondInput = document.getElementById("siffra2");
+
+
+function kalkylator() {
+
+    let inputValue1 = sparaSiffra1();
+    // console.log(inputValue1);
     
-// function valdOperator() {
-//     let operator = document.getElementById("operatör").value;
-//     console.log(operator);
-// }
+    let inputValue2 = sparaSiffra2();
+    // console.log(inputValue2);
 
-// function sparadSiffraTvå() {
-
-//     let siffraTvå = document.getElementById("siffra2").value;
-//     // alert("The user typed '" + siffraTvå + "'");
-//     console.log(siffraTvå);
-// }
-
-
-let firstInput = document.getElementById("siffra1");
-
-
-let operator = document.getElementById("operator");
-
-let secondInput = document.getElementById("siffra2");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function kalkylator() {
-//     // let firstInput = document.getElementById("siffra1");
-//     // let inputValue1 = input.value;
-//     // let secondInput = document.getElementById("siffra2");
-//     // let inputValue2 = input.value;
+    let firstValue = Number(inputValue1)
+    let secondValue = Number(inputValue2)
+    
+    let operatorIKalkylator = operator1();
     
 
-//     let inputValue1 = firstInput;
-//     console.log(inputValue1);
+    if (isNaN(firstValue) || isNaN(secondValue)) {
+       alert("Felaktig inmatning. Var vänlig mata in giltiga nummer.");
+           return;
+    }
     
-//     let inputValue2 = secondInput;
-//     console.log(inputValue2);
-
-
-
-//     if (isNaN(inputValue1) || isNaN(inputValue2)) {
-//        alert("Felaktig inmatning. Var vänlig mata in giltiga nummer.");
-//            return;
-//     }
+    let result;
     
-
-//     let firstValue = Number(inputValue1)
-//     let secondValue = Number(inputValue2)
-    
-//     let operatorIKalkylator = operator;
-    
-    
-    
-//     let result;
-    
-    
-//     switch (operator) {
-//        case '+':
-//            result = firstValue + secondValue;
-//        break;
-//        case '-':
-//            result = firstValue - secondValue;
-//        break;
-//        case '/':
-//            if (secondValue === 0) {
-//                alert('Kan inde dividera med noll!')
-//                return;
-//            }
-//            result = firstValue / secondValue;
-//        break;
-//        case '*':
-//            result = firstValue * secondValue;
-//        break;
-//        case '%':
-//         result = firstValue % secondValue;
-//       break;
-//       case '**':
-//         if  (secondValue === 0) {
-//             alert(`${firstInput} upphöjd med ${secondInput} är lika med noll.`)
-//             return;
-//         } else if (firstInput === 0) {
-//             alert(`${secondValue} upphöjd med ${firstInput} är lika med noll.`)
-//             return;
-//         }
-//          result = firstValue ** secondValue;  
-//       break;
-//        default:
-//        alert('ogiltigt räknesätt, försök igen')
-//        return;
-//     }
+    switch (operatorIKalkylator) {
+       case '+':
+           result = firstValue + secondValue;
+       break;
+       case '-':
+           result = firstValue - secondValue;
+       break;
+       case '/':
+           if (secondValue === 0) {
+               alert('Kan inde dividera med noll!')
+               return;
+           }
+           result = firstValue / secondValue;
+       break;
+       case '*':
+           result = firstValue * secondValue;
+       break;
+       case '%':
+        result = firstValue % secondValue;
+      break;
+      case '**':
+        if  (secondValue === 0) {
+            alert(`${firstInput} upphöjd med ${secondInput} är lika med noll.`)
+            return;
+        } else if (firstInput === 0) {
+            alert(`${secondValue} upphöjd med ${firstInput} är lika med noll.`)
+            return;
+        }
+         result = firstValue ** secondValue;  
+      break;
+       default:
+       alert('ogiltigt räknesätt, försök igen')
+       return;
+    }
     
     
-//     alert(`Resultatet av ${firstInput} ${operator} ${secondValue} är: ${result}`);
+    alert(`Resultatet av ${firstValue} ${operatorIKalkylator} ${secondValue} är: ${result}`);
     
-//     console.log(result);
+    console.log(result);
     
     
-//     }
+    }
     
