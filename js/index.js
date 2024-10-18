0442a4e73750fd65f9391aeaf7874e74a8f10752
ZanaPK0 <timezana10@gmail.com>
2024-10-18 10:36:02 +0200

    vi ska testa lagra inputs.

--- js/index.js
+++ js/index.js
@@ -1,55 +1,122 @@
-function kalkylator() {
-    let firstInput = parseFloat(prompt("första siffran"));
-    let secondInput = parseFloat(prompt("andra siffran"));
+// function sparadSiffraEtt() {
+//     let siffraEtt = document.getElementById("siffra1").value;
+//     // alert("The user typed '" + siffraEtt + "'");
+//     console.log(siffraEtt);
+// }
+    
+// function valdOperator() {
+//     let operator = document.getElementById("operatör").value;
+//     console.log(operator);
+// }
+
+// function sparadSiffraTvå() {
+
+//     let siffraTvå = document.getElementById("siffra2").value;
+//     // alert("The user typed '" + siffraTvå + "'");
+//     console.log(siffraTvå);
+// }
+
+
+let firstInput = document.getElementById("siffra1");
+
+
+let operator = document.getElementById("operator");
+
+let secondInput = document.getElementById("siffra2");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function kalkylator() {
+//     // let firstInput = document.getElementById("siffra1");
+//     // let inputValue1 = input.value;
+//     // let secondInput = document.getElementById("siffra2");
+//     // let inputValue2 = input.value;
+    
+
+//     let inputValue1 = firstInput;
+//     console.log(inputValue1);
+    
+//     let inputValue2 = secondInput;
+//     console.log(inputValue2);
+
+
+
+//     if (isNaN(inputValue1) || isNaN(inputValue2)) {
+//        alert("Felaktig inmatning. Var vänlig mata in giltiga nummer.");
+//            return;
+//     }
+    
+
+//     let firstValue = Number(inputValue1)
+//     let secondValue = Number(inputValue2)
+    
+//     let operatorIKalkylator = operator;
     
     
-    if (isNaN(firstInput) || isNaN(secondInput)) {
-       alert("Felaktig inmatning. Var vänlig mata in giltiga nummer.");
-           return;
-    }
+    
+//     let result;
     
     
+//     switch (operator) {
+//        case '+':
+//            result = firstValue + secondValue;
+//        break;
+//        case '-':
+//            result = firstValue - secondValue;
+//        break;
+//        case '/':
+//            if (secondValue === 0) {
+//                alert('Kan inde dividera med noll!')
+//                return;
+//            }
+//            result = firstValue / secondValue;
+//        break;
+//        case '*':
+//            result = firstValue * secondValue;
+//        break;
+//        case '%':
+//         result = firstValue % secondValue;
+//       break;
+//       case '**':
+//         if  (secondValue === 0) {
+//             alert(`${firstInput} upphöjd med ${secondInput} är lika med noll.`)
+//             return;
+//         } else if (firstInput === 0) {
+//             alert(`${secondValue} upphöjd med ${firstInput} är lika med noll.`)
+//             return;
+//         }
+//          result = firstValue ** secondValue;  
+//       break;
+//        default:
+//        alert('ogiltigt räknesätt, försök igen')
+//        return;
+//     }
     
     
-     let firstValue = Number(firstInput)
-    let secondValue = Number(secondInput)
+//     alert(`Resultatet av ${firstInput} ${operator} ${secondValue} är: ${result}`);
+    
+//     console.log(result);
     
     
-    let operation = prompt("välj en operation (+, -, /, *)")
+//     }
     
-    
-    let result;
-    
-    
-    switch (operation) {
-       case '+':
-           result = firstInput + secondInput;
-       break;
-       case '-':
-           result = firstInput - secondInput;
-       break;
-       case '/':
-           if (secondInput === 0) {
-               alert('Kan inde dividera med noll!')
-               return;
-           }
-           result = firstInput / secondInput;
-       break;
-       case '*':
-           result = firstInput * secondInput;
-       break;
-       default:
-       alert('ogiltigt räknesätt, försök igen')
-       return;
-    }
-    
-    
-    alert(`Resultatet av ${firstInput} ${operation} ${secondInput} är: ${result}`);
-    
-    console.log(result);
-    
-    
-    }
-    
-    
-    kalkylator()
