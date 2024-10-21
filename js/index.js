30e099ee58fa540986f2a45ab13dff01023919f1
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2024-10-21 12:11:04 +0200

    Kommentarer tillagda och dokumentet sparat

--- js/index.js
+++ js/index.js
@@ -27,18 +27,24 @@ function kalkylator() {
     
     let inputValue2 = sparaSiffra2();
     // console.log(inputValue2);
+    //sparar siffran till en input
 
     let firstValue = Number(inputValue1)
     let secondValue = Number(inputValue2)
-    
+    //omvandlar inputen till ett nummer värde
+
+
     let operatorIKalkylator = operator1();
+    //sparar operator functionens resultat till en variabel som används i switch statementet
     
 
     if (isNaN(firstValue) || isNaN(secondValue)) {
        alert("Felaktig inmatning. Var vänlig mata in giltiga nummer.");
            return;
     }
-    
+    //kontrollerar så att variablarna är nummer.
+
+
     let result;
     
     switch (operatorIKalkylator) {
@@ -78,11 +84,14 @@ function kalkylator() {
        alert('ogiltigt räknesätt, försök igen')
        return;
     }
+    //Utför kalkulationen baserat på vilken operatör som valdes.
+    // else if satserna kontrollerar så att siffrorna går att räkna med
     
     
     alert(`Resultatet av ${firstValue} ${operatorIKalkylator} ${secondValue} är: ${result}`);
     console.log(`Resultatet av ${firstValue} ${operatorIKalkylator} ${secondValue} är: ${result}`)
     console.log(result);
+    // matar ut resultatet i en pop up vy och i consol logen för utvecklare och vanliga användare
     
     }
     
